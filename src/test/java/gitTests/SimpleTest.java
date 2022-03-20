5f92bbd1ada886559d8d99ec63b78c452c8a4566
Ilya Besedin <besedin.work@gmail.com>
2022-03-20 18:05:49 +0300

    init commit

---
+++ src/test/java/gitTests/SimpleTest.java
@@ -0,0 +1,13 @@
+package gitTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+
+    @Test
+    void assertTestTrue () {
+        assertTrue (5 > 2);
+    }
+}
